5d07907b46893b8afd1d6222283a5c6ea63c4c6a
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-25 12:57:23 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const authLink =  setContext((_, { headers }) => {
   };
 });
 
-const httpLink = new HttpLink({ uri: 'https://evening-shelf-31048.herokuapp.com/' });
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
